23d3a6bcacaa8ea3ab31d79bd446e57c5071fdfd
Kieran Eglin <kieran.eglin@gmail.com>
2017-03-01 15:42:38 -0800

    set up Laniakea

--- src/angular/controllers/main.js
+++ src/angular/controllers/main.js
@@ -1,3 +1,13 @@
+// Must use old syntax since this is going directly to the view and not being transpiled
+const Laniakea = require('laniakea');
+
 angular.module('foo').controller('fooCtrl', function ($scope) {
     $scope.greet = 'Hello!';
 });
+
+let l = new Laniakea();
+try {
+  l.renameFile();
+} catch (e) {
+  console.log(e.message);
+}
