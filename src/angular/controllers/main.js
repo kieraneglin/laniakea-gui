7a69818b234be1539d43fcbf48c830d401433c62
Kieran Eglin <kieran.eglin@gmail.com>
2017-03-01 18:08:06 -0800

    Roughed in table UI for showing source files

--- src/angular/controllers/main.js
+++ src/angular/controllers/main.js
@@ -1,16 +1,29 @@
 // Must use old syntax since this is going directly to the view and not being transpiled
 const Laniakea = require('laniakea');
 const { dialog } = require('electron').remote;
+const jetpack = require('fs-jetpack');
 
 angular.module('laniakeaGUI').controller('homeCTRL', function ($scope) {
   let l = new Laniakea();
+  $scope.sourceFiles = [];
 
   $scope.updateSourcePath = function () {
-    $scope.sourcePath = dialog.showOpenDialog({
-      filters: [
-        { name: 'ROMs', extensions: l.validFiletypes() }
-      ],
+    dialog.showOpenDialog({
+      filters: [{
+        name: 'ROMs',
+        extensions: l.validFiletypes()
+      }],
       properties: ['openFile', 'openDirectory']
+    }, function (files) {
+      $scope.$apply(function () {
+        $scope.sourcePath = files[0];
+
+        if(jetpack.exists(files[0]) === 'dir'){
+          $scope.sourceFiles = l.listFiles(files[0], true);
+        } else if (jetpack.exists(files[0]) === 'file') {
+          $scope.sourceFiles = files;
+        }
+      });
     });
   };
 
